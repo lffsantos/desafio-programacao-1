715db6571802c008081a93090ff59ec9823d7e45
lucas <lffsantos@gmail.com>
2016-01-08 13:05:59 -0300

    refactor code

--- desafio/core/views.py
+++ desafio/core/views.py
@@ -1,10 +1,11 @@
+import csv
 import unicodedata
 from desafio.core.forms import UploadForm
 from desafio.core.models import UploadFile
 from django.db.models import Sum, F
-from django.http import HttpResponseRedirect
-from django.shortcuts import render, resolve_url as r
+from django.shortcuts import render
 from django.utils.timezone import now
+from six import StringIO
 
 
 def home(request):
@@ -18,26 +19,31 @@ def billing_file(import_name):
 
 def save_file(request):
     form = UploadForm(request.POST, request.FILES)
-    if form.is_valid():
-        data = request.FILES['file']
-        file_name = data.name
-        import_name = file_name + str(now())[:19]
-        data = data.read().decode('utf-8').split('\n')
-        list_content_file = []
-        for line in data[1:]:
-            upload_file = UploadFile()
-            # Normalize content line
-            line = unicodedata.normalize('NFKD', line).encode('ASCII', 'ignore').decode('utf-8').lower().split('\t')
-            upload_file.unpack_line(tuple(line), import_name)
-            list_content_file.append(upload_file)
+    if not form.is_valid():
+        return render(request, 'upload.html', {'form': UploadForm()})
 
-        UploadFile.bulk_insert(list_content_file)
+    data = request.FILES['file']
+    file_name = data.name
+    import_name = file_name + str(now())[:19]
+    data = data.read().decode('utf-8').split('\n')
+    list_content_file = []
+    for line in data[1:]:
+        reader = csv.DictReader(StringIO(line), delimiter='\t', quoting=csv.QUOTE_NONE)
+        fields = tuple(unicodedata.normalize('NFKD', r)
+                           .encode('ASCII', 'ignore')
+                           .decode('utf-8').lower() for r in reader.fieldnames)
+        upload_file = UploadFile()
+        upload_file.unpack_line(fields, import_name)
+        list_content_file.append(upload_file)
 
-        #calcula receita bruta
-        value_billing = billing_file(import_name)
-        return render(request, 'upload.html', {'form': form, 'receita': value_billing, 'file_name': file_name})
+    UploadFile.bulk_insert(list_content_file)
 
-    return render(request, 'upload.html', {'form': UploadForm()})
+    #calcula receita bruta
+    value_billing = billing_file(import_name)
+
+    return render(request, 'upload.html', {'form': form, 'receita': value_billing, 'file_name': file_name})
+
+
 
 def upload(request):
     form = UploadForm()
